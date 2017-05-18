95b1595813c233511bd9c3c1e44a18c06d5f61a1
Chris Barton <c.barton@adelaide.edu.au>
2017-05-18 12:41:40 +0930

    Add create secret method, ran into issue with auth header not being pulled or used in the api client .. seems strange, force set it .. for now

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,8 @@ use UniversityOfAdelaide\OpenShift\Configuration;
 use UniversityOfAdelaide\OpenShift\ApiClient;
 use UniversityOfAdelaide\OpenShift\Api\Core_v1Api;
 use UniversityOfAdelaide\OpenShift\Api\OapiApi;
+use UniversityOfAdelaide\OpenShift\Model\V1ObjectMeta;
+use UniversityOfAdelaide\OpenShift\Model\V1Secret;
 
 /**
  * Class Client
@@ -68,9 +70,12 @@ class Client
         $this->configuration->setApiKeyPrefix('Token', 'Bearer');
         $this->configuration->setApiKey('Token', $token);
         if ($devMode) {
-            $this->configuration->setSSLVerification(FALSE);
-            $this->configuration->setDebug(TRUE);
+          $this->configuration->setSSLVerification(FALSE);
+          $this->configuration->setDebug(TRUE);
         }
+        // There is nothing that retrieves the api key or anything in the api client
+        // @todo - this sucks .. why is this the case. Force set the header for now.
+        $this->configuration->addDefaultHeader('Authorization',  $this->configuration->getApiKeyPrefix('Token') . ' ' . $this->configuration->getApiKey('Token'));
 
         // Store the current working namespace.
         $this->namespace = $namespace;
@@ -79,6 +84,34 @@ class Client
         $this->baseClient = new ApiClient($this->configuration);
         $this->coreClient = new Core_v1Api($this->baseClient);
         $this->openshiftClient = new OapiApi($this->baseClient);
+    }
+
+  /**
+   * Create a namespaced secret.
+   *
+   * @param string $name Name of the secret.
+   * @param array $data Key Value array of secret data. This will base64
+   * encoded.
+   * @return \UniversityOfAdelaide\OpenShift\Model\V1Secret
+   */
+    public function createSecret($name, array $data) {
+
+      // base64 the data
+      foreach ($data as $key => $value) {
+        $data[$key] = base64_encode($value);
+      }
+
+      $secret = new V1Secret([
+        'api_version' => 'v1',
+        'kind' => 'Secret',
+        'metadata' => new V1ObjectMeta([
+          'name' => $name
+        ]),
+        'type' => 'Opaque',
+        'data' => $data
+      ]);
+
+      return $this->coreClient->createCoreV1NamespacedSecret($this->namespace, $secret, TRUE);
     }
 
 }
